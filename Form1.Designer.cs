d41b59db5966ff336c144e02f465e98bb31acf41
Amr_Mu <amr.mero9757.32@gmail.com>
2026-05-02 10:56:04 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.txtTaskDate = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbImportant = new System.Windows.Forms.CheckBox();
+            this.btnDeleteTask = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.titleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importancyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unFinishedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finishedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Basket.ico");
+            this.imageList1.Images.SetKeyName(1, "EmptyBasket.ico");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "Basket.ico");
+            this.imageList2.Images.SetKeyName(1, "EmptyBasket.ico");
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(171, 15);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(171, 20);
+            this.txtTaskName.TabIndex = 1;
+            // 
+            // txtTaskDate
+            // 
+            this.txtTaskDate.Location = new System.Drawing.Point(171, 48);
+            this.txtTaskDate.Name = "txtTaskDate";
+            this.txtTaskDate.ReadOnly = true;
+            this.txtTaskDate.Size = new System.Drawing.Size(171, 20);
+            this.txtTaskDate.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(34, 74);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(205, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TaskTitle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(31, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Date";
+            // 
+            // cbImportant
+            // 
+            this.cbImportant.AutoSize = true;
+            this.cbImportant.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbImportant.Location = new System.Drawing.Point(386, 18);
+            this.cbImportant.Name = "cbImportant";
+            this.cbImportant.Size = new System.Drawing.Size(117, 25);
+            this.cbImportant.TabIndex = 6;
+            this.cbImportant.Text = " Important Task";
+            this.cbImportant.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteTask
+            // 
+            this.btnDeleteTask.BackColor = System.Drawing.Color.Red;
+            this.btnDeleteTask.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnDeleteTask.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDeleteTask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteTask.ForeColor = System.Drawing.Color.Black;
+            this.btnDeleteTask.Location = new System.Drawing.Point(537, 19);
+            this.btnDeleteTask.Name = "btnDeleteTask";
+            this.btnDeleteTask.Size = new System.Drawing.Size(122, 28);
+            this.btnDeleteTask.TabIndex = 8;
+            this.btnDeleteTask.Text = "Delete Selected Task";
+            this.btnDeleteTask.UseVisualStyleBackColor = false;
+            this.btnDeleteTask.Click += new System.EventHandler(this.btnDeleteTask_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OliveDrab;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkKhaki;
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.YellowGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(419, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = " Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.finishedToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.propertiesToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(128, 92);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.titleToolStripMenuItem,
+            this.dateToolStripMenuItem,
+            this.importancyToolStripMenuItem,
+            this.unFinishedToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // titleToolStripMenuItem
+            // 
+            this.titleToolStripMenuItem.Name = "titleToolStripMenuItem";
+            this.titleToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.titleToolStripMenuItem.Text = "Title...";
+            this.titleToolStripMenuItem.Click += new System.EventHandler(this.titleToolStripMenuItem_Click);
+            // 
+            // dateToolStripMenuItem
+            // 
+            this.dateToolStripMenuItem.Name = "dateToolStripMenuItem";
+            this.dateToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.dateToolStripMenuItem.Text = "Date...";
+            this.dateToolStripMenuItem.Click += new System.EventHandler(this.dateToolStripMenuItem_Click);
+            // 
+            // importancyToolStripMenuItem
+            // 
+            this.importancyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yesToolStripMenuItem,
+            this.noToolStripMenuItem});
+            this.importancyToolStripMenuItem.Name = "importancyToolStripMenuItem";
+            this.importancyToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.importancyToolStripMenuItem.Text = "Important?";
+            // 
+            // yesToolStripMenuItem
+            // 
+            this.yesToolStripMenuItem.Name = "yesToolStripMenuItem";
+            this.yesToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.yesToolStripMenuItem.Text = "Yes";
+            this.yesToolStripMenuItem.Click += new System.EventHandler(this.yesToolStripMenuItem_Click);
+            // 
+            // noToolStripMenuItem
+            // 
+            this.noToolStripMenuItem.Name = "noToolStripMenuItem";
+            this.noToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.noToolStripMenuItem.Text = "No";
+            this.noToolStripMenuItem.Click += new System.EventHandler(this.noToolStripMenuItem_Click);
+            // 
+            // unFinishedToolStripMenuItem
+            // 
+            this.unFinishedToolStripMenuItem.Name = "unFinishedToolStripMenuItem";
+            this.unFinishedToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.unFinishedToolStripMenuItem.Text = "UnFinished";
+            this.unFinishedToolStripMenuItem.Click += new System.EventHandler(this.unFinishedToolStripMenuItem_Click);
+            // 
+            // finishedToolStripMenuItem
+            // 
+            this.finishedToolStripMenuItem.Name = "finishedToolStripMenuItem";
+            this.finishedToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.finishedToolStripMenuItem.Text = "Finished";
+            this.finishedToolStripMenuItem.Click += new System.EventHandler(this.finishedToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.propertiesToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            this.Title.Width = 302;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 153;
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.Date});
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(12, 146);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(924, 348);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
+            this.listView1.BeforeLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_BeforeLabelEdit);
+            this.listView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 506);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDeleteTask);
+            this.Controls.Add(this.cbImportant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtTaskDate);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.TextBox txtTaskDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbImportant;
+        private System.Windows.Forms.Button btnDeleteTask;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finishedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem titleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importancyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unFinishedToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTaskDate.Text) || string.IsNullOrEmpty(txtTaskName.Text))
+            {
+                MessageBox.Show("Title or Date is Empty Please Fill Them First!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ListViewItem item = new ListViewItem(txtTaskName.Text.Trim());
+            item.SubItems.Add(txtTaskDate.Text.Trim());
+
+            item.ImageIndex = 1;
+
+            if (cbImportant.Checked == true)
+            {
+                item.ForeColor = Color.Red;
+
+                item.ImageIndex = 0;
+                
+            }
+            item.Tag = 0;
+
+            listView1.Items.Add(item);
+            
+            dateTimePicker1.Value = DateTime.Now;
+            cbImportant.Checked = false;
+            txtTaskName.Clear();
+            txtTaskDate.Clear();
+            txtTaskName.Focus();
+
+            Notification();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            txtTaskDate.Text = dateTimePicker1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void btnDeleteTask_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+            
+                if (item.Checked == true)
+                {
+                    
+                    item.Remove();
+                }
+            }
+            
+        }
+        
+        private void listView1_MouseUp(object sender, MouseEventArgs e) // تخصيص  contex menue  للايتمز
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                ListViewItem item = listView1.GetItemAt(e.X, e.Y);
+                if (item != null) //  تتاكد اذا الماوس علس ايتم مش مكان فرغ
+                {
+
+                    contextMenuStrip1.Show(listView1, e.Location);
+                    if (item.Tag.ToString() != "1")
+                    {
+                        finishedToolStripMenuItem.Enabled = true; // اغلاق خيار ال finished  اذا كان مختار بافعل من قبل
+                    }
+                    else
+                    {
+                        finishedToolStripMenuItem.Enabled = false;
+                    }
+
+                }
+
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(item);
+            }
+        }
+
+
+
+        private void finishedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+
+                ((ToolStripMenuItem)sender).Enabled = true;
+                
+                    item.ForeColor = Color.RosyBrown;
+                    item.Text += "   (Finished)✔";
+                    item.BackColor = Color.Pink;
+                    item.Tag = 1;
+
+                
+
+            }
+        }
+
+        private void titleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.SelectedItems[0].BeginEdit();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select Item!");
+            }
+
+
+        }
+
+        private void listView1_BeforeLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            
+        }
+
+        private void listView1_AfterLabelEdit(object sender, LabelEditEventArgs e) // حدث يتم بعد انتهاء التعديل
+        {
+            if (e.Label == null || e.Label.Trim() == "") // اذا كان التايتل فارغ نلغي التعديل
+            {
+                MessageBox.Show("You Cant Let It Empty!");
+
+                e.CancelEdit = true;
+            }
+           
+        }
+
+        private void dateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Tag.ToString() != "1") 
+            {
+
+                listView1.SelectedItems[0].SubItems[1].Text = dateTimePicker1.Text;
+            }
+        }
+
+        private void yesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Tag.ToString() != "1")
+            {
+                listView1.SelectedItems[0].ForeColor = Color.Red;
+                listView1.SelectedItems[0].ImageIndex = 0;
+            }
+        }
+
+        private void noToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Tag.ToString() != "1")
+            {
+                listView1.SelectedItems[0].ForeColor = Color.Black;
+                listView1.SelectedItems[0].ImageIndex = 1;
+            }
+        }
+
+        private void unFinishedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Tag.ToString() == "1")
+            {
+                listView1.SelectedItems[0].Tag = 0;
+                listView1.SelectedItems[0].ForeColor = Color.Black;
+                listView1.SelectedItems[0].BackColor= Color.Transparent;
+
+                string S = listView1.SelectedItems[0].Text;
+                listView1.SelectedItems[0].Text = S.Remove(S.Length - 14, 14).Trim(); // (Finished)✔ حذف كلمة 
+            }
+        }
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            string txt = item.Text;
+            string Date = item.SubItems[1].Text;
+            string Importancy = item.ForeColor != Color.Red ? "Normal" : "Important";
+            MessageBox.Show($"Title: {txt}\n" + $"Date:{Date}\n" + $"Importancy: {Importancy}\n", "Properties", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+        }
+
+        private void Notification()
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "Task Info!";
+            notifyIcon1.BalloonTipText = "New task has added successfully ✔😉";
+
+            notifyIcon1.ShowBalloonTip(2000);
+
+
+        }
+    }
+   
+}
